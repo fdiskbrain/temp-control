c79b695ca79bc720ee7c0320ce04c8691f194785
FB <zhoronghi@gmail.com>
2021-10-07 14:24:14 +0800

    mv files

--- full_control/temp_control.h
+++ full_control/temp_control.h
@@ -1,11 +1,10 @@
 // RGB
-#define MAX_SIZE 20
 #define Max_LED 3
+#define MAX_SIZE 32
 //
 #define CPU_USAGE_PATH "/proc/stat"
 #define HARDWARE_PATH "/proc/device-tree/model"
 #define TEMP_PATH "/sys/class/thermal/thermal_zone0/temp"
-#define MAX_SIZE 32
 
 int fd_i2c;
 void setRGB(int num, int R, int G, int B);
